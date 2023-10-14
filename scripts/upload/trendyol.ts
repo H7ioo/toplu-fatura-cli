a75955390292bd5ca9efd90422c405e1ca09864d
Omar Jano <omar.jano05@gmail.com>
2023-10-14 18:40:36 +0300

    fixed: trendyol token expired after 90'th request

--- scripts/upload/trendyol.ts
+++ scripts/upload/trendyol.ts
@@ -10,8 +10,6 @@ import { readFile } from "fs/promises";
 import { sleep } from "../../lib/utils";
 
 const GOTO_URL = "https://partner.trendyol.com/orders/shipment-packages/all";
-// invoiceNumber
-// invoiceDateTime
 const invoiceURL = (invoiceId: string) =>
   `https://sellerpublic-mars.trendyol.com/order-core-sellercenterordersbff-service/shipment-packages/${invoiceId}/customer-invoice`;
 
@@ -21,40 +19,6 @@ export async function trendyolUpload(date: string) {
     headless: false,
     userDataDir: "./puppeteer/user_data",
   });
-
-  const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto(GOTO_URL);
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Login
-  const emailInput =
-    "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > div.email-phone-input-wrapper.g-mt-20.g-mb-20 > div > div > div > div > input[type=text]";
-
-  const emailInputExists = await page.$(emailInput);
-  if (emailInputExists) {
-    await page.waitForSelector(emailInput);
-    await page.focus(emailInput);
-    await page.keyboard.type(env.TRENDYOL_EMAIL);
-    const passwordInput =
-      "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > div.password-input-wrapper.g-mb-20 > div.password.g-input > div > div > div > input[type=password]";
-    await page.waitForSelector(passwordInput);
-    await page.focus(passwordInput);
-    await page.keyboard.type(env.TRENDYOL_PASSWORD);
-
-    const loginButton =
-      "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > button > div";
-    await page.waitForSelector(loginButton);
-    await page.click(loginButton);
-  }
-
-  const authToken = (await page.cookies()).filter(
-    (c) => c.domain === "partner.trendyol.com" && c.name === "auth_token"
-  )[0]?.value;
-  if (!authToken) throw new Error("Auth token not found.");
 
   const invoicesFile = `./data/trendyol/${date}/invoices.json`;
 
@@ -66,58 +30,96 @@ export async function trendyolUpload(date: string) {
   const file = await readFile(invoicesFile, "utf8");
   const invoices: Invoice[] = JSON.parse(file);
 
-  for (let index = 0; index < invoices.length; index++) {
-    const invoice = invoices[index];
-    if (!invoice) throw new Error("Invoice doesn't exist?");
+  const chunkSize = 50;
+  for (let i = 0; i < invoices.length; i += chunkSize) {
+    const invoicesChunk = invoices.slice(i, i + chunkSize);
+    const page = await browser.newPage();
 
-    try {
-      const fileStream = fs.createReadStream(invoice.filePath);
-      if (!fileStream) throw new Error("File doesn't exist");
+    // Navigate the page to a URL
+    await page.goto(GOTO_URL);
 
-      const formData = new FormData();
-      formData.append("file", fileStream);
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
-      const res = await axios.post(
-        invoiceURL(invoice.packageNumber.toString()),
-        formData,
-        {
-          headers: {
-            authorization: `Bearer ${authToken}`,
-            "Content-Type": "multipart/form-data",
-            Accept: "application/json, text/plain, */*",
-            authority: "sellerpublic-mars.trendyol.com",
-            origin: "https://partner.trendyol.com",
-          },
-          params: invoice.isExport
-            ? {
-                invoiceNumber: invoice.invoiceNumber,
-                invoiceDateTime: invoice.orderTimestamp,
-              }
-            : undefined,
-        }
-      );
+    // Login
+    const emailInput =
+      "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > div.email-phone-input-wrapper.g-mt-20.g-mb-20 > div > div > div > div > input[type=text]";
 
-      await sleep(1000 * (Math.random() * 5));
+    const emailInputExists = await page.$(emailInput);
+    if (emailInputExists) {
+      await page.waitForSelector(emailInput);
+      await page.focus(emailInput);
+      await page.keyboard.type(env.TRENDYOL_EMAIL);
+      const passwordInput =
+        "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > div.password-input-wrapper.g-mb-20 > div.password.g-input > div > div > div > input[type=password]";
+      await page.waitForSelector(passwordInput);
+      await page.focus(passwordInput);
+      await page.keyboard.type(env.TRENDYOL_PASSWORD);
 
-      if (res.status >= 200 && res.status < 300) {
-        console.log(
-          `${invoice.packageNumber} fatura başarıyla yüklendi. Index: ${
-            index + 1
-          }/${invoices.length}`
+      const loginButton =
+        "#app-wrapper > div > div.auth.with-header-footer > div > div > div.login.g-d-flex > div.form > div.login-form > div > form > button > div";
+      await page.waitForSelector(loginButton);
+      await page.click(loginButton);
+    }
+
+    const authToken = (await page.cookies()).filter(
+      (c) => c.domain === "partner.trendyol.com" && c.name === "auth_token"
+    )[0]?.value;
+    if (!authToken) throw new Error("Auth token not found.");
+
+    for (let index = 0; index < invoicesChunk.length; index++) {
+      const invoice = invoicesChunk[index];
+      if (!invoice) throw new Error("Invoice doesn't exist?");
+
+      try {
+        const fileStream = fs.createReadStream(invoice.filePath);
+        if (!fileStream) throw new Error("File doesn't exist");
+
+        const formData = new FormData();
+        formData.append("file", fileStream);
+
+        const res = await axios.post(
+          invoiceURL(invoice.packageNumber.toString()),
+          formData,
+          {
+            headers: {
+              authorization: `Bearer ${authToken}`,
+              "Content-Type": "multipart/form-data",
+              Accept: "application/json, text/plain, */*",
+              authority: "sellerpublic-mars.trendyol.com",
+              origin: "https://partner.trendyol.com",
+            },
+            params: invoice.isExport
+              ? {
+                  invoiceNumber: invoice.invoiceNumber,
+                  invoiceDateTime: invoice.orderTimestamp,
+                }
+              : undefined,
+          }
         );
-      }
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        logger.error(error.message, error);
-        if (error.status === 400) {
-          const err: AxiosError<invoicePostError> = error;
-          logger.error(
-            `Error uploading invoice: ${err.response?.data.errors[0]?.message}`,
-            err
+
+        await sleep(1000 * (Math.random() * 5));
+
+        if (res.status >= 200 && res.status < 300) {
+          console.log(
+            `${invoice.packageNumber} fatura başarıyla yüklendi. Index: ${
+              index + 1
+            }/${invoices.length}`
           );
         }
-      } else if (error instanceof Error) {
-        logger.error(error.message, error);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          logger.error(error.message, error);
+          if (error.status === 400) {
+            const err: AxiosError<invoicePostError> = error;
+            logger.error(
+              `Error uploading invoice: ${err.response?.data.errors[0]?.message}`,
+              err
+            );
+          }
+        } else if (error instanceof Error) {
+          logger.error(error.message, error);
+        }
       }
     }
   }
